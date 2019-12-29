dd54724930d7fed58122408a99aeea4591b39a09
cky66666666 <chaikangyu1996@icloud.com>
2019-12-29 17:08:33 +0800

    first edition

---
+++ result.m
@@ -0,0 +1,34 @@
+{(gs^2*((4*I)*e^2*gs^4*(4*e^2 + MH^2 + (-4*e^2 + MH^2)*Cos[th])^2*Csc[th/2]^4*
+     Eps[Momentum[p[1]], Momentum[p[3]], Momentum[p[5]], Momentum[p[6]]] - 
+    (4*e^2 - MH^2)*(15*e^2*gs^4*(-4*e^2 + MH^2)^2 + 
+      27*(-48*e^4 + 8*e^2*MH^2 + MH^4)*Pi^2*v^2*yb^2 + 
+      (20*e^2*gs^4*(32*e^4 - 8*e^2*MH^2 + MH^4))/(-1 + Cos[th]) + 
+      (-4*e^2 + MH^2)^2*(5*e^2*gs^4 + 27*Pi^2*v^2*yb^2)*Cos[th] + 
+      864*e^4*Pi^2*v^2*yb^2*Sec[th/2]^2 + (4*I)*Sqrt[2]*e*gs^4*
+       (Eps[Momentum[p[1]], Momentum[p[2]], Momentum[p[3]], Momentum[p[5]]] - 
+        Eps[Momentum[p[1]], Momentum[p[2]], Momentum[p[3]], Momentum[p[6]]])*
+       ((8*e^2 - 2*MH^2)*Cot[th/2] + MH^2*Csc[th/2]^4*Sin[th]))))/
+  (144*e^2*(-4*e^2 + MH^2)^2*Pi^2*v^2), 
+ (gs^2*((3*e^2*gs^4*(-4*e^2 + MH^2)^2 + 15*(-48*e^4 + 8*e^2*MH^2 + MH^4)*Pi^2*
+       v^2*yb^2 + (4*e^2*gs^4*(32*e^4 - 8*e^2*MH^2 + MH^4))/(-1 + Cos[th]) + 
+      (-4*e^2 + MH^2)^2*(e^2*gs^4 + 15*Pi^2*v^2*yb^2)*Cos[th] + 
+      (960*e^4*Pi^2*v^2*yb^2)/(1 + Cos[th]))/(e^2*(4*e^2 - MH^2)*Pi^2*v^2) - 
+    (12*I)*yb^2*((2*(Eps[Momentum[p[1]], Momentum[p[3]], Momentum[p[5]], 
+          Momentum[p[6]]] - Eps[Momentum[p[1]], Momentum[p[4]], 
+          Momentum[p[5]], Momentum[p[6]]] + Eps[Momentum[p[3]], 
+          Momentum[p[4]], Momentum[p[5]], Momentum[p[6]]]))/e^2 - 
+      (4*((4*e^2 - MH^2)*Eps[Momentum[p[1]], Momentum[p[2]], Momentum[p[5]], 
+           Momentum[p[6]]] + (4*e^2 + MH^2)*Eps[Momentum[p[1]], 
+           Momentum[p[3]], Momentum[p[5]], Momentum[p[6]]] + 
+         4*e^2*Eps[Momentum[p[2]], Momentum[p[3]], Momentum[p[5]], 
+           Momentum[p[6]]] + MH^2*Eps[Momentum[p[3]], Momentum[p[4]], 
+           Momentum[p[5]], Momentum[p[6]]]))/((4*e^4 - e^2*MH^2)*
+        (1 + Cos[th])) - (16*MH^2*Eps[Momentum[p[2]], Momentum[p[3]], 
+         Momentum[p[5]], Momentum[p[6]]]*Sec[th/2]^4)/(-4*e^2 + MH^2)^2 + 
+      (64*Sqrt[2]*Csc[th]^3*(-Eps[Momentum[p[1]], Momentum[p[2]], 
+           Momentum[p[3]], Momentum[p[5]]] + Eps[Momentum[p[1]], 
+          Momentum[p[2]], Momentum[p[3]], Momentum[p[6]]])*Sin[th/2]^4)/
+       (4*e^3 - e*MH^2) + (Sqrt[2]*(-Eps[Momentum[p[1]], Momentum[p[3]], 
+           Momentum[p[4]], Momentum[p[5]]] + Eps[Momentum[p[1]], 
+          Momentum[p[3]], Momentum[p[4]], Momentum[p[6]]])*Tan[th/2])/e^3)))/
+  48}
